1b7abf33ae2fe3c1432360c865bf2b8b74be0b98
FSWD2329 GUNASUNDARY <gunasundaryboopalan@gmail.com>
2025-12-26 22:35:08 +0400

    update all files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Navbar from "./components/Navbar.js"
+import Search from "./components/Search.js";
+import Products from "./components/Products.js";
+import About from "./components/About.js";
+import Footer from "./components/Footer.js";
+
+function App()
+{
+   return( <div> <Navbar></Navbar>
+ <Search></Search>
+ <Products></Products>
+ <About></About>
+ <Footer></Footer></div>)
+}
+export default App
---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.js"
+import "./style.css"
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+//creating navbar component
+
+//search)
+
+   
+   //products
+   
+   //About
+  
+   //footer
+  
+
+root.render(
+ <App></App>
+)
